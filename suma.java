a329ba4487d65893a715bae0e2b3eaebb328cf47
Jose Luis S. L. <founder@bitnarius.com>
2025-03-30 16:52:31 -0700

    Create suma.java
    Suma de dos números enteros

---
+++ suma.java
@@ -0,0 +1,12 @@
+//Suma de dos numeros enteros
+public class suma {
+public static void main(String[] args){
+
+int numero1 = 5;
+int numero2 = 4;
+
+int suma = numero1 + numero2;
+
+System.out.println("El valor de la suma es igual a: " + suma);
+   }
+}
